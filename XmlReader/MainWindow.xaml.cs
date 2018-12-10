c8d45c97cdeee50fb174962d3bde93f01c13c29a
Rafaat Mir <rmir@pnimedia.com>
2018-12-09 21:29:20 -0800

    Sample XML File push

--- XmlReader/MainWindow.xaml.cs
+++ XmlReader/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +23,31 @@ namespace XmlReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public TextBox XmlKey;
+        private string FILE_PATH = "this.xml";
+        private static DataSet _dsSet;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void ParseXML(object o, EventArgs e)
+        {
+            XmlValues.Text = "Loading...";
+            if (_dsSet == null || _dsSet.Tables.Count == 0)
+            {
+                var xmlString = File.ReadAllText(FILE_PATH);
+                var stringReader = new StringReader(xmlString);
+                _dsSet = new DataSet();
+                _dsSet.ReadXml(stringReader);
+            }
+
+            var values = _dsSet.Tables[XmlKey.Text];
+            var toDisplay = XmlKey.Text;
+            XmlValues.Text = Convert.ToString(values);
+
+            return;
         }
     }
 }
